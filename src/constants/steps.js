187298a26b4a8ba4074c948251e0ad541e211472
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-05-29 14:52:47 +0100

    feat: create form steps constant variables

---
+++ src/constants/steps.js
@@ -0,0 +1,34 @@
+const STEPS = {
+  STEP_1: "step 1: personal info",
+  STEP_2: "step 2: choose plan",
+  STEP_3: "step 3: pick addons",
+  STEP_4: "step 4: confirm setup",
+  STEP_5: "step 5: success message",
+};
+
+const STEPS_LIST = [
+  {
+    id: STEPS.STEP_1,
+    step: 1,
+    title: "your info",
+  },
+  {
+    id: STEPS.STEP_2,
+    step: 2,
+    title: "choose plan",
+  },
+
+  {
+    id: STEPS.STEP_3,
+    step: 3,
+    title: "add-ons",
+  },
+
+  {
+    id: STEPS.STEP_4,
+    step: 4,
+    title: "summary",
+  },
+];
+
+export { STEPS, STEPS_LIST };
